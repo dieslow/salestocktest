4a61845ee206bfc71454583dafc28dc85015d3a9
dieslow <tanzahabe@gmail.com>
2016-03-02 06:32:23 +0700

    Edit Index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'SaleStock - Abe' });
+	res.render('index', { title: 'SaleStock - Tanzah Abe' });
 });
 
 module.exports = router;
